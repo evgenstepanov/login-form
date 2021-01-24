996f65ee5721e3742368d25c69c14ed23bf8173a
Evgeny Stepanov <evgenos@list.ru>
2021-01-24 21:27:21 +0300

    зависимость стилей Header от стейта

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { StoreContext } from '../store/store';
 import logo from '../assets/logo.svg';
 
 export default function Header() {
+  const {
+    isEntered: [isEntered],
+  } = React.useContext(StoreContext);
   return (
-    <div className="header">
-      <img className="logo header__logo" src={logo} alt="" />
+    <div className={`header${isEntered ? ' header_in-system' : ''}`}>
+      <img
+        className={`logo header__logo ${
+          isEntered ? 'header__logo_in-system' : ''
+        }`}
+        src={logo}
+        alt="logo"
+      />
       <h1 className="name header__name">
         Корпоративная информационная система
       </h1>
