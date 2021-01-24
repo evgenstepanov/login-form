3c7c08f2eb9a37cf6e3d7cef58f07c2a153ab104
Evgeny Stepanov <evgenos@list.ru>
2021-01-24 21:26:02 +0300

    основная разметка формы

--- src/App.js
+++ src/App.js
@@ -4,19 +4,37 @@ import FormLogin from './components/FormLogin';
 import { StoreContext } from './store/store';
 import FormRestorePassword from './components/FormRestorePassword';
 import './index.css';
+import loader from './assets/loader.gif';
 
 function App() {
   const {
     scrolled: [scrolled],
+    isEntered: [isEntered],
   } = React.useContext(StoreContext);
 
   return (
     <div className="container">
       <Header />
-      <div className="forms-box">
-        <div className={`forms-container${scrolled ? ' scrolled' : ''}`}>
+      <div
+        className={`forms-box${scrolled ? ' forms-box_scrolled' : ''}${
+          isEntered ? ' forms-box_in-system' : ''
+        }`}
+      >
+        <div
+          className={`forms-container${
+            scrolled ? ' forms-container_scrolled' : ''
+          }`}
+        >
           <FormLogin />
           <FormRestorePassword />
+        </div>
+        <div className={`greeting${isEntered ? ' greeting_in-system' : ''}`}>
+          {isEntered ? (
+            <>
+              <p className="greeting__text">Добрый день, Владислав!</p>
+              <img className="greeting__loader" src={loader} alt="" srcSet="" />
+            </>
+          ) : null}
         </div>
       </div>
     </div>
